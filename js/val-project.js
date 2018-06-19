5a2ac112e7f6924d8343fd1932493f4e030f1fc6
vitaaachen <39292092+vitaaachen@users.noreply.github.com>
2018-06-20 01:27:27 +0800

    V2.5.0

---
+++ js/val-project.js
@@ -0,0 +1,38 @@
+var valPorject = [
+    {
+        "d-val-porject":"大發金服 活動頁",//專案名稱
+        "d-val-category":{//專案分類
+            "d-val-category0":"網站",
+            "d-val-category1":"獨立站點",
+            "d-val-category2":"金融",
+            "d-val-category3":"活動網站",
+        },
+        "d-val-skill":[//使用/支援技術
+            {"image" : "img/icon/icon-html-red2.png", "title": "HTML5/HTML"},
+            {"image" : "img/icon/icon-js-red.png"   , "title": "JavaScript"},
+            {"image" : "img/icon/icon-css2.png"     , "title": "CSS3/CSS"},
+            {"image" : "img/icon/icon-jq-red.png"   , "title": "jQuery"}
+        ],
+        "d-val-charge":"網頁開發（切版,功能,特效等）。",//負責項目
+        "d-val-exp":"這個網站比較有挑戰性的地方大概是滾動數字的部分以及程式設定的部分，程式是設定每日的累積人數以及每日更新所剩數量，以JavaScript撰寫。此站為單頁式，配合視覺做出來的網站。不使用第三方套件寫出一個功能和特效是非常需要有一定基礎的，這個網站是很好的練習。",//專案心得
+        "d-val-link":"",//查看網站
+    },
+    {
+        "d-val-porject":"大發金服 活動頁",//專案名稱
+        "d-val-category":{//專案分類
+            "d-val-category0":"網站",
+            "d-val-category1":"獨立站點",
+            "d-val-category2":"金融",
+            "d-val-category3":"活動網站",
+        },
+        "d-val-skill":[//使用/支援技術
+            {"image" : "img/icon/awdawd.png", "title": "HTML5/HTML"},
+            {"image" : "img/icon/aw-js-red.png"   , "title": "JavaScriadawdawdpt"},
+            {"image" : "img/icon/icon-css2.png"     , "title": "CSS3/CawdawSS"},
+            {"image" : "img/icon/icon-jq-red.png"   , "title": "jQuadwawdery"}
+        ],
+        "d-val-charge":"網頁開發（切版,功能,特效等）。",//負責項目
+        "d-val-exp":"這個網站比較有挑戰性的地方大概是滾動數字的部分以及程式設定的部分，程式是設定每日的累積人數以及每日更新所剩數量，以JavaScript撰寫。此站為單頁式，配合視覺做出來的網站。不使用第三方套件寫出一個功能和特效是非常需要有一定基礎的，這個網站是很好的練習。",//專案心得
+        "d-val-link":"",//查看網站
+    }
+]
