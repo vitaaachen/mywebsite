88e81b96de2da725fd3eac7f472f43a84a0cb02b
ciaociao <ciaochocho@gmail.com>
2019-01-03 10:33:04 +0800

    V2019-1
    壓縮圖片/新增作品

--- js/val-project.js
+++ js/val-project.js
@@ -1,5 +1,6 @@
 var valPorject = [
     {
+        "d-val-no":"0",
         "d-val-porject": "大發金服 活動頁",//專案名稱
         "d-val-category": [//專案分類
             { "category": "網站" },
@@ -15,9 +16,10 @@ var valPorject = [
         "d-val-charge": "網頁開發（切版,功能,特效等）。",//負責項目
         "d-val-exp": "這個網站比較有挑戰性的地方是滾動數字程式設定的部分，程式是設定每日的累積人數以及每日更新所剩數量，以 JavaScript 撰寫。此站為單頁式，配合視覺做出來的網站。不使用第三方套件寫出一個功能和特效是非常需要有一定基礎的，這個網站是很好的練習。",//專案心得
         "d-val-link": "./website/dafaginfu/activity/a_dafa.html",//查看網站
-        "d-val-cover": "./img/work/dafa-act/a.png",
+        "d-val-cover": ["./img/work/dafa-act/a.png",]
     },
     {
+        "d-val-no":"1",
         "d-val-porject": "AI人工智能彩票整合平台系统",
         "d-val-category": [
             { "category": "網站" },
@@ -36,9 +38,10 @@ var valPorject = [
         "d-val-charge": "Wordpress樣式環境調整,切版,外掛調用,JQ和JS功能製作,網站管理,網站維護。",
         "d-val-exp": "配合各個部門以及較長的行程規劃。我主要負責前端功能製作、切版、Wordpress 外掛設定與串接。製作時，因為等待的時間較長，我會先將資料與所需工具準備好，加快工作效率。因為不是第一次做大型的 Wordpress 網站所以在外掛的連接與環境調整也會比較上手。比較值得一提的是影片的部分，為了做出自動播放影片的效果查了不少資料，後來才發現原來是 Chrome 有更改使用者設定。經過這次的專案感覺解決問題的的能力又提升了一格呢。",
         "d-val-link": "http://www.aicpbw.com/",
-        "d-val-cover": "./img/work/g2e/a.png",
+        "d-val-cover": ["./img/work/g2e/a.png",]
     },
     {
+        "d-val-no":"2",
         "d-val-porject": "新濠皇璽會",
         "d-val-category": [
             { "category": "非原創" },
@@ -58,9 +61,10 @@ var valPorject = [
         "d-val-charge": "切版,JQ與JS製作。",
         "d-val-exp": "這是我到公司第一個製作的網站，因為只有我一個前端工程師，很多功能和細節都是自己慢慢摸索，也因為都是靠自己製作所以也學習到很多。像是搜尋資料的效率與獨立解決問題的方法，以前不懂得優化自己的工作速度，現在要做什麼功能都知道可以用方法可以最快完成目標，是收穫很多的一個專案。",
         "d-val-link": "./website/club/index.html",
-        "d-val-cover": "./img/work/crown/a.png",
+        "d-val-cover": ["./img/work/crown/a.png",]
     },
     {
+        "d-val-no":"3",
         "d-val-porject": "UltraEGaming 奧創娛樂",
         "d-val-category": [
             { "category": "網站" },
@@ -79,9 +83,10 @@ var valPorject = [
         "d-val-charge": "Wordpress 樣式環境調整,切版,外掛調用,JQ和JQ功能製作,網站管理與維護。",
         "d-val-exp": "娛樂奕資訊站，主要在推廣平台產品和發文章，這是我出社會後第一個大型專案，學習到跨部門溝通，例如：如何將自己的技術與不熟此領域的人溝通，互相傾聽彼此的建議以及互相幫忙配合來完成專案。這個站點使用許多Wordpress的外掛所以我也學習到如何使用各個插件串接。",
         "d-val-link": "https://www.ultraegaming.com",
-        "d-val-cover": "./img/work/ug/a.png",
+        "d-val-cover": ["./img/work/ug/a.png",]
     },
     {
+        "d-val-no":"4",
         "d-val-porject": "點點盈",
         "d-val-category": [
             { "category": "非原創" },
@@ -99,9 +104,10 @@ var valPorject = [
         "d-val-charge": "切版,JQ和JS功能製作,卷軸動畫設定。",
         "d-val-exp": "這個專案是仿製網站的練習作品。學習如何製作出當滾輪滾動的時候播放動畫的效果，製作出一個比較精細的網站。隨然這個站沒有RWD稍嫌可惜，但是他每個區塊都算蠻細膩的，尤其是最牛金願的部分我處理到很多細節。",
         "d-val-link": "./website/dotdot/index.html",
-        "d-val-cover": "./img/work/dot/a.png",
+        "d-val-cover": ["./img/work/dot/a.png",]
     },
     {
+        "d-val-no":"5",
         "d-val-porject": "錢進牛股",
         "d-val-category": [
             { "category": "網站" },
@@ -120,9 +126,10 @@ var valPorject = [
         "d-val-charge": "切版,JQ與JS製作。",
         "d-val-exp": "這個網站經過兩次改版，最後才演變成今天的樣子。在 SEO 部門會需要做大量的推廣網站，為了增加瀏覽器的收錄。比較特別的地方是這個網站手機和電腦分為兩個網頁，不是以 RWD 的方式下去切。原因是希望手機版可以呈現像 APP 的樣子為了不要讓頁面太長讓使用者感到乏味，只列出最精簡的功能。又礙於兩個版本樣子相差太多所以製作成兩個網頁依照不同解析度去切換是最快的。這樣子的做法也算是蠻有趣的。",
         "d-val-link": "./website/cowstock/index.html",
-        "d-val-cover": "./img/work/stock/a.jpg",
+        "d-val-cover": ["./img/work/stock/a.png",]
     },
     {
+        "d-val-no":"6",
         "d-val-porject": "88老虎",
         "d-val-category": [
             { "category": "非原創" },
@@ -141,9 +148,10 @@ var valPorject = [
         "d-val-charge": "Wordpress 樣式環境調整,適應不同架構之網站調整,切版,外掛調用,JQ和JQ功能製作,網站管理。",
         "d-val-exp": "這個網站一共做了兩種架構，分別是 Wordpress 和獨立靜態網站。以 Wordpress 為後台來製作此站，需要調整 Wordpress 整體樣式後再串接外掛等功能來製作。另一個是獨立的靜態網站即是一般的寫法，兩種架構皆由我負責製作。手機版是直接跳去別的頁面，所以此網站並沒有RWD。為什麼會需要分成兩種架構的原因是，我們沒有後台人員，需要依靠 Wordpress 的功能來替網站做每日 Update。",
         "d-val-link": "./website/88tiger/index.html",
-        "d-val-cover": "./img/work/tiger/a.png",
+        "d-val-cover":["./img/work/tiger/a.png",]
     },
     {
+        "d-val-no":"7",
         "d-val-porject": "UltraEGaming 廣告專案",
         "d-val-category": [
             { "category": "網站" },
@@ -163,9 +171,13 @@ var valPorject = [
         "d-val-charge": "HTML+CSS切版。",
         "d-val-exp": "為了提供 UltraEGaming 品牌的廣告方案所製作的網站，所以直接附屬在 UltraEGaming 底下。與視覺配合獨立製作，如果要說遇到困難的話，大概就是表格的問題，表格在手機呈現方式有限，要怎麼設定讓手機可以呈現完整內容又不會造成使用者的不便正是這次的研究項目之一。",
         "d-val-link": "https://www.ultraegaming.com/adservices.html",
-        "d-val-cover": "./img/work/ug-ad/a.png",
+        "d-val-cover":[
+            "./img/work/ug-ad/a.png",
+            "./img/work/ug-ad/3.png" 
+        ]
     },
     {
+        "d-val-no":"8",
         "d-val-porject": "大發金服 導覽頁",
         "d-val-category": [
             { "category": "網站" },
@@ -180,9 +192,10 @@ var valPorject = [
         "d-val-charge": "MIP技術研究,切版,優化網頁利於SEO。",
         "d-val-exp": "此站特別的地方是依照 SEO 部門的需求以 MIP（Mobile Instant Pages）語法架構下去切版製作，為了要讓大陸的手機瀏覽器能夠加速下載此網站。使用 MIP 架構切版有先研究1-3天的時間，了解此語法的規則並改用他們的方式來寫網站。雖然功能上有所限制，因為這個架構不接受客製化的 JS，但是以一般的網站來說這些語法還是夠用的。",
         "d-val-link": "./website/dafaginfu/mip/guide.html",
-        "d-val-cover": "./img/work/dafa-mip/a.png",
+        "d-val-cover": ["./img/work/dafa-mip/a.png",]
     },
     {
+        "d-val-no":"9",
         "d-val-porject": "大發金服 文章推廣",
         "d-val-category": [
             { "category": "網站" },
@@ -196,9 +209,10 @@ var valPorject = [
         "d-val-charge": "HTML+CSS切版。",
         "d-val-exp": "推廣網站。此站是為了要推廣大發金服品牌所做的網頁，由視覺設計好頁面後再由我這邊做切版實體化的動作。因為是做大陸市場的關西，整個風格和物件皆以大陸那邊習慣為主。",
         "d-val-link": "./website/dafaginfu/article/article01-v2.html",
-        "d-val-cover": "./img/work/dafa/a.png",
+        "d-val-cover": ["./img/work/dafa/a.png",]
     },
     {
+        "d-val-no":"10",
         "d-val-porject": "錢進牛股 金融論壇",
         "d-val-category": [
             { "category": "網站" },
@@ -212,9 +226,10 @@ var valPorject = [
         "d-val-charge": "Discuz 論壇研究,大陸論壇資訊收集,HTML,CSS技術支援區塊。",
         "d-val-exp": "這個網站是我第一次使用 Discuz 架構來製作網站，所以花了一段時間研究後台的用法和一些論壇相關的資訊。這個網站我主要負責前端版塊的配置，Discuz 論壇本身自己就有工具可以切區塊而我所要做的是針對切出來的區塊做樣式和細節的調整。這個論壇不支援在前台自己寫 JQ 和 JS 會被判定為異常，所以任何互動的呈現皆須使用 CSS 達成，是蠻有挑戰的一個項目。",
         "d-val-link": "http://ngu888.com/forum.php",
-        "d-val-cover": "./img/work/financial/a.png",
+        "d-val-cover": ["./img/work/financial/a.png",]
     },
     {
+        "d-val-no":"11",
         "d-val-porject": "百家樂娛樂城",
         "d-val-category": [
             { "category": "獨立製作" },
@@ -234,9 +249,10 @@ var valPorject = [
         "d-val-charge": "網頁視覺設計,製圖 (Banner) ,切版,管理網站。",
         "d-val-exp": "這個專案的視覺和網站開發是由我獨立製作。以往都是我和視覺互相配合製作網站這次由於時間較緊迫的緣故改將網站的設計由我負責，在製作的過程中因為設計和開發都是我自己所以更能明白區塊如何表達節省了不少時間。為了能夠提供更彈性的管理和擴充方式，我在製作的時候有特別規範 CSS 和 HTML 讓網站在編修的時候更容易新增。 ",
         "d-val-link": "./website/baccarat/index.html",
-        "d-val-cover": "./img/work/baccarat/a.png",
+        "d-val-cover": ["./img/work/baccarat/a.png",]
     },
     {
+        "d-val-no":"12",
         /*暫時隱藏*/
         "d-val-porject": "鳳凰國際",
         "d-val-category": [
@@ -257,9 +273,10 @@ var valPorject = [
         "d-val-charge": "Wordpress 樣式環境調整,切版與外掛調用",
         "d-val-exp": "宣導活動時間與介紹各個主播的網站，配合視覺切版行成此站。此站，無 Demo 因為已經收了，在Wordpress的架構所以只有截圖沒有檔案可供下載。",
         "d-val-link": "沒有",
-        "d-val-cover": "./img/work/bird/0.png",
+        "d-val-cover": ["./img/work/bird/0.png",]
     },
     {
+        "d-val-no":"13",
         "d-val-porject": "和信智慧產權事務所",
         "d-val-category": [
             { "category": "網站" },
@@ -278,9 +295,10 @@ var valPorject = [
         "d-val-charge": "網站設計,切版,客戶的需求討論,進度確認與修改,JS與JQ設定。",
         "d-val-exp": "在製作和信網站的時候，花了很多時間跟客戶確認需求和討論他們所期待的樣子，雙方達到共識後才開始進行。在製作這個網站的時候有運用新的技術，像是判斷網址決定頁面顯示內容和切換效果，是用JQ寫。",
         "d-val-link": "http://www.he-xin.com.tw/index.html",
-        "d-val-cover": "./img/work/he-xin/a.png",
+        "d-val-cover": ["./img/work/he-xin/a.png",]
     },
     {
+        "d-val-no":"14",
         "d-val-porject": "皇璽會娛樂城",
         "d-val-category": [
             { "category": "非原創" },
@@ -300,6 +318,26 @@ var valPorject = [
         "d-val-charge": "網站設計,素材統整,切版,特效製作,JQ設定。",
         "d-val-exp": "在製作這個網站的時候處理了一些細節，像是菜單滑動的時候考慮到高度過高可能會妨礙閱讀而縮小的調動與TOP前往最上層的按鈕在滑動的時候才出來等細節。有了這些經驗，讓我在製作網站的時候更能站在使用者的角度去製作網站，給使用者一個良好的網站體驗。",
         "d-val-link": "website/iwni/index.html",
-        "d-val-cover": "./img/work/signature/a.png",
+        "d-val-cover": ["./img/work/signature/a.png",]
+    },
+    {
+        "d-val-no":"15",
+        "d-val-porject": "優博UEO",
+        "d-val-category": [
+            { "category": "現金網平台" },
+            { "category": "娛樂" },
+            { "category": "RWD" },
+        ],
+        "d-val-skill": [
+            { "image": "img/icon/icon-html-red2.png", "title": "HTML5/HTML" },
+            { "image": "img/icon/icon-css2.png", "title": "CSS3/CSS" },
+            { "image": "img/icon/icon-js-red.png", "title": "JavaScript" },
+            { "image": "img/icon/icon-jq-red.png", "title": "jQuery" },
+            { "image": "img/icon/icon-rwd.png", "title": "RWD" },
+        ],
+        "d-val-charge": "RWD切版,使用者互動,UI/UX,網頁速度優化。",
+        "d-val-exp": "這個網站有兩個版本，所有的網頁畫面皆由我一人製作，包含使用者的互動效果、瀏覽器相容問題與RWD等，php資料串接由其他人負責。※網址demo為第一版網站，圖片為第二版網站。",
+        "d-val-link": "website/new_tf_skin/index.html",
+        "d-val-cover": ["./img/work/ueo/pc-index.png","./img/work/ueo/pc-slot.png","./img/work/ueo/pc-fishing.png","./img/work/ueo/pc-sport.png","./img/work/ueo/pc-user.png","./img/work/ueo/pc-record.png"]
     },
 ]
